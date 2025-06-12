0ec940f6b06953f40f2b000d48c245b5feb3b836
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-06-12 14:26:00 +0900

    開発環境整備

--- src/extension.ts
+++ src/extension.ts
@@ -2,47 +2,37 @@ import * as vscode from "vscode";
 
 export async function activate(context: vscode.ExtensionContext) {
 	const config = vscode.workspace.getConfiguration();
+	console.log(vscode.extensions.all);
+
 	const extensions = vscode.extensions.all;
+
 	const extensionIds = extensions
 		.map((ext) => ext.id)
-		.filter(
-			(id) =>
-				!/^vscode\./.test(id) &&
-				id !== "AkimeAki.devcontainer-install-local-extensions",
-		);
-	const settingIds = config.get(
-		"dev.containers.defaultExtensionsIfInstalledLocally",
-	) as string[];
+		.filter((id) => !/^vscode\./.test(id) && id !== "AkimeAki.devcontainer-install-local-extensions");
+	const settingIds = config.get("dev.containers.defaultExtensionsIfInstalledLocally") as string[];
+
+	console.log(settingIds);
 
 	// 設定されているけど、アンインストールされた拡張機能
-	const deletedExtensionIds = settingIds.filter(
-		(id) => !extensionIds.includes(id),
-	);
+	const deletedExtensionIds = settingIds.filter((id) => !extensionIds.includes(id));
 
 	// インストールされている拡張機能の中で設定されていない拡張機能
-	const notSetExtensionIds = extensionIds.filter(
-		(id) => !settingIds.includes(id),
-	);
+	const notSetExtensionIds = extensionIds.filter((id) => !settingIds.includes(id));
 
 	// インストールされている拡張機能と設定されている拡張機能に差分がある場合に処理を実行
 	if (deletedExtensionIds.length !== 0 || notSetExtensionIds.length !== 0) {
 		await config.update(
 			"dev.containers.defaultExtensionsIfInstalledLocally",
 			extensionIds,
-			vscode.ConfigurationTarget.Global,
+			vscode.ConfigurationTarget.Global
 		);
 
-		const addedSettingsMessage =
-			notSetExtensionIds.length !== 0
-				? ` 追加: ${notSetExtensionIds.join(",")}`
-				: "";
+		const addedSettingsMessage = notSetExtensionIds.length !== 0 ? ` 追加: ${notSetExtensionIds.join(",")}` : "";
 		const deletedSettingsMessage =
-			deletedExtensionIds.length !== 0
-				? ` 削除: ${deletedExtensionIds.join(",")}`
-				: "";
+			deletedExtensionIds.length !== 0 ? ` 削除: ${deletedExtensionIds.join(",")}` : "";
 
 		vscode.window.showInformationMessage(
-			`ローカルにインストールされている全ての拡張機能を開発コンテナでも使用できるようにしました。${addedSettingsMessage}${deletedSettingsMessage}`,
+			`ローカルにインストールされている全ての拡張機能を開発コンテナでも使用できるようにしました。${addedSettingsMessage}${deletedSettingsMessage}`
 		);
 	}
 
